c7cf0775d53d745b7e24f602d81640399350ab54
Yadushree E <155658481+Yadushree7@users.noreply.github.com>
2025-11-06 23:13:35 +0530

    Update Room.jsx

--- Frontend/src/components/Room.jsx
+++ Frontend/src/components/Room.jsx
@@ -585,7 +585,7 @@ function Room({ username, room, socket }) {
                     marginBottom: "8px",
                     // backgroundColor: "black",
                     // background: "linear-gradient(to right, black 50%, #8B0000 75% , #FF0000)",
-                    background: "linear-gradient(to right, #000000 0%, #1a0000 20%, #330000 40%, #660000 60%, #990000 80%, #FF0000 100%)",
+                    background: "linear-gradient(to left, #000000 0%, #1a0000 20%, #330000 40%, #660000 60%, #990000 80%, #FF0000 100%)",
 
                     color: "white",
                     borderRadius: "8px",
@@ -685,7 +685,7 @@ function Room({ username, room, socket }) {
               style={{
               flex: 1,
               borderRadius: "10px",
-              backgroundColor: "#FFB8B8",
+              backgroundColor: "#750000",
               // background: "radial-gradient(circle, #FF0000, #990000, #660000, #330000, #000000)", 
               padding: "10px",
               overflowY: "auto",
@@ -718,7 +718,7 @@ function Room({ username, room, socket }) {
                     maxWidth: "70%",
                     border: "1px solid red",
                     backgroundColor:
-                      msg.username === username ? "#000000" : " #FF0000",
+                      msg.username === username ? "#black" : " #red",
                     // background: msg.username === username
                     //   ? "linear-gradient(to right, #000000 0%, #1a0000 20%, #330000 40%, #660000 60%, #990000 80%, #FF0000 100%)"
                     //   : "linear-gradient(to left, #000000 0%, #1a0000 20%, #330000 40%, #660000 60%, #990000 80%, #FF0000 100%)",
