1c2fcb95e1dd85417a73c4f81be53ce69434eb67
Yadushree E <155658481+Yadushree7@users.noreply.github.com>
2025-11-07 00:22:50 +0530

    Update Lobby.jsx

--- Frontend/src/components/Lobby.jsx
+++ Frontend/src/components/Lobby.jsx
@@ -60,7 +60,7 @@ function Lobby({ setUsername, setRoom, setJoined, socket }) {
         backgroundColor: "#252522",
       }}
     >
-      <h1 className="text-black">Welcome!</h1>
+      <h1 style={{color:"black"}}>Welcome!</h1>
       <input
         value={localRoom}
         onChange={handleRoomChange}
@@ -107,7 +107,7 @@ function Lobby({ setUsername, setRoom, setJoined, socket }) {
       >
         Join
       </button>
-      <a href="#" onClick={fetchRoomID} className="text-black">
+      <a href="#" onClick={fetchRoomID} style={{color:"black"}}>
         Generate Unique Room ID
       </a>
     </div>
